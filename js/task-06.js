3cfa962ca46485b42aa0b75d731017c4f7be9e38
Marina-Lis <105066555+Marina-Lis@users.noreply.github.com>
2022-11-19 19:52:31 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -31,7 +31,7 @@ const textInput = document.querySelector("#validation-input");
 
 
 textInput.addEventListener("blur", () => {
-    if(textInput.value.length>=textInput.dataset.length){
+    if(textInput.value.length===textInput.dataset.length){
         textInput.classList.add("valid");
         textInput.classList.remove("invalid");
     }
